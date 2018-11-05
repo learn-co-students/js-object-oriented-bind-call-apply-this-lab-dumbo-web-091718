9cffc16de7d654658505b483324453024246f4a3
github username <github email address>
2018-11-05 00:32:40 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
 //Your code here
+
+// let obect1{proreties: cbCity()}  <-- Methods --> let obect2{proreties: cbCity()}
+//
+
+
+const obj = {num:2};
+const addToThis = function maFunc(a) {
+  return this.num + a;
+}
+console.log(addToThis.call(obj, 3))  // maFunc.call obj && maFunc init arg
+
+
+
+const justInvoke = function(fn){
+  return fn()
+}
+
+const setThisWithCall = function(fn, thisValue, arg){
+  return fn.call(thisValue,arg)
+}
+
+function setThisWithApply(fn, thisValue, args){
+  return fn.apply(thisValue, args)
+}
+
+function returnNewFunctionOf(fnTobeCopied, thisValue){
+  let GrandMasCookingFunc =  fnTobeCopied.bind(thisValue)
+  return GrandMasCookingFunc
+}
